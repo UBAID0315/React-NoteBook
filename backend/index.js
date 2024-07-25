6e3c7edd6dc9e803ae76d079f7f6c1922ec88980
Ubaid Ur Rehman <119524246+UBAID0315@users.noreply.github.com>
2024-07-25 17:21:39 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,3 @@
 const connecttoMongo = require('./db')
-const express = require('express')
 
 connecttoMongo();
-const app = express()
-const port = 3000;
-
-// Available Routes (default)
-// app.get('/' ,(req,res)=>{
-//     res.send('hello ubaid!')}
-// )
-
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
-
-app.listen(port ,()=>{
-    console.log(`example app listening port is http://localhost:${port}`)
-})
