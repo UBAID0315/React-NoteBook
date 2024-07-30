6e9000dfd98d8afee99a8f3a2c72a9ddc863b0ec
Ubaid Ur Rehman <119524246+UBAID0315@users.noreply.github.com>
2024-07-30 21:24:50 +0500

    Creating a User

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,9 +4,10 @@ const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const fetchuser = require('../middleware/fetchUser')
 const JWT_SECRET = "thisismysecretcode@123";
 
+// Router 1: (for creating the user)
 router.get('/createuser', [
 
     body('name', 'Enter a valid name').isLength({ min: 3 }),
@@ -21,8 +22,8 @@ router.get('/createuser', [
 
     // use try and catch for handle unwanted errors
     try {
-        
-        // if same user is exists never add hi
+
+        // if same user is exists never add it
         let user = await User.findOne({ email: req.body.email })
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email has already exists!" })
@@ -30,27 +31,25 @@ router.get('/createuser', [
 
         // Generating the hash password
         const salt = await bcrypt.genSalt(10)
-        const securePassword = await bcrypt.hash(req.body.password,salt)
-        
+        const securePassword = await bcrypt.hash(req.body.password, salt)
+
         // Creating a User
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
             password: securePassword
         })
-        
+
         const data = {
-            user:{
-                id:user.id
+            user: {
+                id: user.id
             }
-        }    
-        const token = jwt.sign(data,JWT_SECRET)        
-        res.json({token})
-    } 
+        }
+        const token = jwt.sign(data, JWT_SECRET)
+        res.json({ token })
+    }
     catch (error) {
         console.error(error.message)
         res.status(500).send("Some error has been occured!");
     }
 })
-
-module.exports = router
