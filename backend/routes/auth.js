52dd1cf099bc63df56b00cada391e383b478f42e
Ubaid Ur Rehman <119524246+UBAID0315@users.noreply.github.com>
2024-07-30 21:25:43 +0500

    Creating a Login for user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -53,3 +53,45 @@ router.get('/createuser', [
         res.status(500).send("Some error has been occured!");
     }
 })
+
+
+
+// Router 2: (to login the user)
+
+router.get('/login', [
+
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').exists(),
+
+], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            return req.status(400).json({ error: "Please enter the correct credentials" })
+        }
+
+        const PasswordCompare = await bcrypt.compare(password, user.password);
+
+        if (!PasswordCompare) {
+            return req.status(400).json({ error: "Please enter the correct credentials" })
+        }
+
+        data = {
+            user: {
+                id: user.id
+            }
+        }
+        const token = jwt.sign(data,JWT_SECRET)
+        res.json({ token })
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some error has been occured!");
+    }
+})
