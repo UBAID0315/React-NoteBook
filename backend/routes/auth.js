218637314ed767e16415f90754ce7f9a01ce75f0
Ubaid Ur Rehman <119524246+UBAID0315@users.noreply.github.com>
2024-07-31 22:57:58 +0500

    Adding and Fetching the Notes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const fetchuser = require('../middleware/fetchUser')
+const jwt = require('jsonwebtoken');
 const JWT_SECRET = "thisismysecretcode@123";
 
 // Router 1: (for creating the user)
-router.get('/createuser', [
+router.post('/createuser', [
 
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
@@ -58,7 +58,7 @@ router.get('/createuser', [
 
 // Router 2: (to login the user)
 
-router.get('/login', [
+router.post('/login', [
 
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').exists(),
@@ -74,13 +74,13 @@ router.get('/login', [
         let user = await User.findOne({ email });
 
         if (!user) {
-            return req.status(400).json({ error: "Please enter the correct credentials" })
+            return res.status(400).json({ error: "Please enter the correct credentials" })
         }
 
         const PasswordCompare = await bcrypt.compare(password, user.password);
 
         if (!PasswordCompare) {
-            return req.status(400).json({ error: "Please enter the correct credentials" })
+            return res.status(400).json({ error: "Please enter the correct credentials" })
         }
 
         data = {
@@ -88,7 +88,7 @@ router.get('/login', [
                 id: user.id
             }
         }
-        const token = jwt.sign(data,JWT_SECRET)
+        const token = jwt.sign(data, JWT_SECRET)
         res.json({ token })
     } catch (error) {
         console.error(error.message)
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,46 @@
 const express = require('express')
 const router = express.Router();
-const notes = require('../models/Notes')
+const Notes = require('../models/Note')
+const { body, validationResult } = require('express-validator');
+const fetchuser = require('../middleware/fetchUser')
 
-router.get('/',(req,res)=>{
-    res.json([])
+// Route 1: Create the notes of a user
+router.post('/addnote', fetchuser, [
+
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Enter a description of minimum length 5').isLength({ min: 5 }),
+
+], async (req, res) => {
+    try {
+        const { title, description, tags } = req.body;
+
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Notes({
+            title, description, tags, user: req.user.id
+        })
+        const saveNote = await note.save()
+        res.json(saveNote)
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some error has been occured!");
+    }
+})
+
+// Route 2: Add or delete notes of user
+
+// Route 3: Fetch all the notes of a specific user
+router.get('/fetchnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Notes.find({ user: req.user.id })
+        res.send(notes)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some error has been occured!");
+    }
 })
 
 module.exports = router
